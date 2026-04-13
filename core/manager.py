abd25069af069f9c9a49bfa39d3ad72cc550d502
irakli <maniaika07@gmail.com>
2026-04-13 04:13:34 +0400

    feat: complete TaskManager implementation with CRUD support

--- core/manager.py
+++ core/manager.py
@@ -1,4 +1,5 @@
 from storage.storage import JSONStorage, Task, BaseStorage
+from typing import Unpack
 
 
 class TaskManager:
@@ -10,16 +11,35 @@ class TaskManager:
         self.storage.save(self.tasks)
 
     def add(self, task: Task) -> None:
+        if any(t["id"] == task["id"] for t in self.tasks):
+            raise ValueError(f"task with id {task['id']} already exists")
+
         self.tasks.append(task)
+        self.save_all()
 
-    def get(self):
-        pass
+    def get(self, id: int) -> Task | None:
+        for task in self.tasks:
+            if task["id"] == id:
+                return task
+        return None
 
-    def list(self):
-        pass
+    def list(self) -> list[Task]:
+        return self.tasks
 
-    def update(self):
-        pass
+    def update(self, id_to_update: int, **updates: Unpack[Task]) -> bool:
+        for task in self.tasks:
+            if task["id"] == id_to_update:
+                task.update(updates)
+                self.save_all()
 
-    def delete(self):
-        pass
+                return True
+        return False
+
+    def delete(self, id: int) -> bool:
+        for i, task in enumerate(self.tasks):
+            if task["id"] == id:
+                del self.tasks[i]
+                self.save_all()
+
+                return True
+        return False
