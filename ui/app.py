eb666d785ad692649ae0f387192227fbd351ca0f
irakli <maniaika07@gmail.com>
2026-04-23 17:34:05 +0400

    feat(todo): connect TaskManager and dynamic task rendering
    - Load tasks from storage via manager.list()
    - Introduce VerticalScroll container for task list

--- ui/app.py
+++ ui/app.py
@@ -1,22 +1,29 @@
 from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll
 from textual.widgets import Header, Footer
-from widgets.task_widget import TaskWidget
+
+from core.storage.task import Task
+from core.manager import BaseTaskManager, TaskManager
+from ui.widgets.task_widget import TaskWidget
 
 
 class TodoApp(App[None]):
     CSS_PATH = [
         "tcss/styles.tcss",
     ]
+    def __init__(self, manager: BaseTaskManager | None = None) -> None:
+        super().__init__()
+        self.manager = manager or TaskManager()
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield VerticalScroll(
-            TaskWidget(),
-        )
-
-
-if __name__ == "__main__":
-    app = TodoApp()
-    app.run()
+        with VerticalScroll():
+            for id, task in self.manager.list().items():
+                yield TaskWidget(
+                    id, 
+                    task["content"],
+                    task["uploaded"].strftime("%d %b %Y • %H:%M"),
+                    task["deadline"].strftime("%d %b %Y • %H:%M"),
+                    task["complete"],
+                )
