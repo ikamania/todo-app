c98c1fa1f7c8ed3016d805f626a89496cd181f99
irakli <maniaika07@gmail.com>
2026-04-23 17:36:17 +0400

    refactor(task-widget): rename content to task_name and update constructor

--- ui/widgets/task_widget.py
+++ ui/widgets/task_widget.py
@@ -4,17 +4,18 @@ from textual.widgets import Button, Checkbox, Label
 
 
 class TaskWidget(HorizontalGroup):
-    def __init__(self, id: int, name: str, created_at: str, deadline: str, complete: bool):
+    def __init__(self, task_id: str, content: str, uploaded: str, deadline: str, complete: bool):
             super().__init__()
-            self.task_name = name
-            self.created_at = created_at
+            self.task_id = task_id
+            self.content = content
+            self.uploaded = uploaded
             self.deadline = deadline
             self.complete = complete
 
     def compose(self) -> ComposeResult:
         yield VerticalGroup(
             HorizontalGroup (
-                Label(self.task_name, classes="name"),
+                Label(self.content, classes="name"),
 
                 HorizontalGroup (
                     Checkbox("complete", classes="complete"),
@@ -24,8 +25,8 @@ class TaskWidget(HorizontalGroup):
             ),
 
             HorizontalGroup (
-                Label(f"Created: {self.created_at}", classes="date"),
-                Label(f"Deadline: {self.deadline}", classes="date"),
+                Label(f"uploaded: {self.uploaded}", classes="date"),
+                Label(f"deadline: {self.deadline}", classes="date"),
                 classes="dates"
             )
         )
